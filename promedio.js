606174128cbf2ae2d53bea72b890a6fb2afee371
Wilfredo Morales <wiljmoralesa@gmail.com>
2021-07-04 15:05:21 -0400

    Promedio con js

---
+++ promedio.js
@@ -0,0 +1,5 @@
+const promedio = (lista) => lista.reduce((a,b) => a + b) / lista.length 
+
+  // let suma = lista.reduce((a,b) => a + b);
+  // let promedio = suma / lista.length;
+  // return promedio; 
